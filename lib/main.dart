9f59032a6608f78c3c3f92b21fbd40670d6255a9
Andrew Thow <thow1976@hotmail.co.uk>
2022-01-11 16:21:42 +0000

    main_dart

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() {
+  runApp(MyHomePage());
+}
+
+@override
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Shop',
+      amount: 20.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter App"),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text("Chart"),
+                    elevation: 5,
+                  )),
+              Card(
+                child: Text("List of Tx"),
+                color: Colors.blue,
+                elevation: 5,
+              )
+            ],
+          )),
+    );
+  }
+}
