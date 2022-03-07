02a6085240690854313a19f56a0851aab04b072f
Andrew Thow <thow1976@hotmail.co.uk>
2022-03-07 20:17:28 +0000

    22_section_4_splitting_widgets_initiating_transactions_list

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -3,26 +3,7 @@ import '../models/transaction.dart';
 
 import 'package:intl/intl.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  State<TransactionList> createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Shop',
-      amount: 20.99,
-      date: DateTime.now(),
-    ),
-  ];
+class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
