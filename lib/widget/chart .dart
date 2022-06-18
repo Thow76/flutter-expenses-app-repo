fd978889f9ff56640d687bce5863f2d30a17e7bc
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-18 14:49:54 +0100

    41_Section_4_Looping_Through_Lists_Creating_List_Variable_And_Constructor

--- lib/widget/chart .dart
+++ lib/widget/chart .dart
@@ -1,9 +1,13 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import '../models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       return {'day': 'T', 'amount': 9.99};
