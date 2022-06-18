27edb5d732f5afe4b61bf15e17ad6b77b8188afd
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-18 19:13:20 +0100

    45_Section_4_Deriving_Recent_Transactions_Creating_Bar_Chart_Class_Field_Variables_Constructor

--- lib/widget/chart_bar.dart
+++ lib/widget/chart_bar.dart
@@ -2,10 +2,12 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
+class ChartBar extends StatelessWidget {
   final String label;
   final double spendingAmount;
   final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
   Widget build(BuildContext context) {}
