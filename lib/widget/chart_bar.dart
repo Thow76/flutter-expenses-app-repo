cd8050b9d8cc9e60d5746d412e35c4faa287e5af
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-18 19:25:43 +0100

    46_Section_4_Deriving_Recent_Transactions_Setting_Up_Bar_Chart_Structure

--- lib/widget/chart_bar.dart
+++ lib/widget/chart_bar.dart
@@ -10,5 +10,21 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: null,
+        ),
+        SizedBox(height: 4),
+        Text(label),
+      ],
+    );
+  }
 }
