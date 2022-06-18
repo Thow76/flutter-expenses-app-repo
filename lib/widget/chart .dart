96b3cbc9fd96215d3a8841578e71d1c55920a296
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-18 14:44:13 +0100

    40_Section_4_Looping_Through_Lists_Generating_Lists

---
+++ lib/widget/chart .dart
@@ -0,0 +1,23 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
