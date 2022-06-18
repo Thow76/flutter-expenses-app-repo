69712470731549098a831ca40272031ba874fee0
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-18 15:06:21 +0100

    42_Section_4_Looping_Through_Lists_Creating_Grouping_List_Elements_With_For_Loop

--- lib/widget/chart .dart
+++ lib/widget/chart .dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -10,7 +11,19 @@ class Chart extends StatelessWidget {
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      return {'day': 'T', 'amount': 9.99};
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
     });
   }
 
