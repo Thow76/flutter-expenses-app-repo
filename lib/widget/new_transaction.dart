021246cbde33dfae11ebb6d47b09cc5993e84542
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-19 09:22:28 +0100

    55_Section_4_Getting_Started_With_Date_Selection_Styling_Buttons_With_Themes

--- lib/widget/new_transaction.dart
+++ lib/widget/new_transaction.dart
@@ -46,18 +46,28 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.number,
               onSubmitted: (_) => submitData(),
             ),
-            Row(
-              children: <Widget>[
-                Text('No Date Chosen'),
-                FlatButton(
-                  child: Text('Choose Date'),
-                  onPressed: () {},
-                )
-              ],
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen'),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
             ),
-            FlatButton(
+            RaisedButton(
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
+              textColor: Theme.of(context).textTheme.button.color,
+              color: Theme.of(context).primaryColor,
               onPressed: submitData,
             ),
           ],
